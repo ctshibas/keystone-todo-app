50a6b136feb75da6aaaf4dc4eeb69700cba0df49
Corneille Tshibasu <corneilletshibasu@gmail.com>
2020-10-01 21:18:58 +0100

    restructuring of the keystone-todo-app repo

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { Text } = require('@keystonejs/fields');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const TodoSchema = require('./lists/Todo.js');
+const UserSchema = require('./lists/Todo.js');
+
+// creating an instance of Keystone with the mongoose adapter to connect project to mongodb
+const keystone = new Keystone({
+  adapter: new MongooseAdapter({ mongoUri: 'mongodb://localhost:27017/keystone-cms-app' }),
+});
+
+// create a Todo list schema - refer to list
+keystone.createList('Todo', TodoSchema);
+keystone.createList('User', UserSchema);
+
+// exporting the driver and the GraphQL interface UI app setup on start of project
+module.exports = {
+  keystone,
+  apps: [
+	new GraphQLApp(),
+	new AdminUIApp({name: 'example-project', enableDefaultRoute: true}),
+  ],
+
+};
---
+++ lists/Todo.js
@@ -0,0 +1,27 @@
+const { Text, CalendarDay, Checkbox } = require('@keystonejs/fields');
+
+// creating the list model
+module.exports = {
+  fields: {
+    description: {
+      	type: Text,
+      	isRequired: true,
+    },
+    isComplete: {
+      	type: Checkbox,
+      	defaultValue: false,
+    },
+    // adding the extra attributes, deadline and assignee
+    deadline: {
+	type: CalendarDay,
+	dateFrom: '2020-01-01',
+	dateTo: '2027-01-01',
+	isRequired: false,
+	defaultValue: new Date().toISOString('YYYY-MM-DD').substring(0, 10), // Today's date
+    },
+    assignee: {
+	type: Text,
+	isRequired: true,
+    },
+  },
+};
